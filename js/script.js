c7d7481543844fc96ade422ae99addde1ce9d512
Carina Carriedo <carriedo.carina@gmail.com>
2016-07-05 10:53:45 -0700

    added files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Put event listener on start button so that when you click on button, you are taken to the game screen
+
+//Clicking on the start button should also trigger the program to randomly select a letter
+
+//Based on the letter that is randomly selected, the program should randomly select one of the three photos in the image database that correspond to that letter.
+
+//The program should also randomly place that photo in one of the three card positions
+
+//The other two card positions should be filled by random selection of photos that do not correspond to the letter
+
+//When the player clicks on the correct photo, a star should appear on the right and they should hear a positive-sounding beep
+
+//After the star and posiive sound, the player should be presented with a new letter and a new set of cards
+
+//When the player clicks on the wrong photo, they should hear a negative-sounding beep and the photo should disappear.
+
+//If the player clicks on the wrong photo again, the same thing should happen so that they are left with the right answer. After that, they should be presented with a new letter and a new set of cards
+
+//The game should continue until the player has collected 5 stars
