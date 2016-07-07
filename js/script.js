82e93ae93af481e5b116a60b56bae4e4536aa697
Carina Carriedo <carriedo.carina@gmail.com>
2016-07-07 13:57:58 -0700

    added styling to start screen

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 var letters = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "y"];
 var images = ['b1.jpg', 'b2.png', 'b3.jpg', 'c1.jpg', 'c2.jpg', 'c3.jpg', 'd1.jpg', 'd2.jpg', 'd3.jpg', 'f1.jpg', 'f2.jpg', 'f3.jpg', 'g1.jpg', 'g2.jpg', 'h1.jpg', 'h2.jpg', 'h3.jpg', 'j1.jpg', 'j2.jpg', 'j3.jpg', 'k1.jpg', 'k2.jpg', 'k3.jpg', 'l1.jpg', 'l2.jpg', 'l3.jpg', 'm1.jpg', 'm2.jpg', 'm3.jpg', 'n1.jpg', 'n2.jpg', 'n3.png', 'p1.jpg', 'p2.jpg', 'p3.jpg', 'r1.jpg', 'r2.jpg', 'r3.jpg', 's1.png', 's2.jpg', 's3.jpg', 't1.png', 't2.jpg', 't3.png', 'v1.jpg', 'v2.jpg', 'v3.jpg', 'w1.jpg', 'w2.jpg', 'w3.jpg', 'y1.jpg', 'y2.jpg', 'y3.jpg' ];
 var $letter;
-
+var playerWins = 0;
 
 //add event listener to Play Now button. When clicked, button disappears, and random letter and images appear.
+
 $(".button").click(function() {
   $(".button").remove();
+  $("#books").remove();
+  $("h1").remove();
   $( "#letter" ).addClass( "active" );
   $(".card").addClass("active");
   $("#stars").addClass("active");
@@ -46,6 +49,7 @@ $(".button").click(function() {
 });
 
 //compare letter to card that is clicked. If matches, star appears and new letter and cards are presented
+
 $(".card").click(function(event){
   var imageString =  $(this).css("background-image");
   var letter = imageString[imageString.length - 8];
@@ -55,6 +59,7 @@ $(".card").click(function(event){
     $('<div class="star" style="width: 150px; height: 100px; background-image: url(img/star297837_640.png);\
     background-size: contain; background-repeat: no-repeat; margin: 20px"></div>').appendTo("#stars");
     $('.card').css('background-image', 'none');
+    playerWins = playerWins + 1;
   } else {
     alert("Sorry. Wrong answer.")
   }
